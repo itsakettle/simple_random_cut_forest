c1b1ac26c1149219710ce9147880de2303a1420a
Will Rogers <itsakettle@gmail.com>
2023-09-12 23:20:10 +0100

    progress

--- random_cut_forest/random_cut_forest.py
+++ random_cut_forest/random_cut_forest.py
@@ -1,13 +1,29 @@
 import numpy as np
-from typing import int
+from typing import NamedTuple, List
+from random_cut_forest.binary_tree import BinaryTree
 
 class RandomCutForest:
     
     data: np.ndarray
+    tree: BinaryTree
+    dim: int
+    n: int
 
-    def __init__(self, max_depth, min_elements):
+    def __init__(self, max_depth: int, data: np.ndarray):
         self.max_depth = max_depth
-        self.min_elements = min_elements
+        self.n, self.dim = data.shape
+        self.data = data
+        self.tree: BinaryTree = BinaryTree(root_node_data=RandomCutForest.Cut(None, None, None))
 
-    def fit(self, data):
+
+    def fit(self):
+        
         pass
+    
+    def make_cut(i:) -> :
+        
+    
+    class Cut(NamedTuple):
+        i: List[int]
+        dim: int
+        threshold: float
--- tests/test_random_cut_forest.py
+++ tests/test_random_cut_forest.py
@@ -1,5 +1,23 @@
 import pytest
+import numpy as np
+from random_cut_forest.random_cut_forest import RandomCutForest
 
+@pytest.fixture
+def data_3_dim():
+    data = [[1.0, 3.0, 2.0], 
+            [33.0, 1.0, 11.0],
+            [3.0, 1.0, 12.0],
+            [4.0, 2.0, -13.0],
+            [5.0, 3.0, 11.0],
+            [6.0, 2.0, 9.0],
+            [7.0, 3.0, 10.0],
+            [8.0, 2.0, 12.0],
+            [9.0, 3.0, 13.0],
+            [8.0, 2.0, 11.0],
+            [7.0, 3.0, 12.0],]
 
-def test_mytest():
-    print("hello")
+    return np.array(data)
+
+def test_mytest(data_3_dim):
+    rcf = RandomCutForest(max_depth=10, data=data_3_dim)
+    
